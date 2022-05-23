55169b930ad62d4fa75a22591a71ff6132506e1a
Abdallah_Abdelsabour <abdallah97158@example.com>
2022-05-23 09:49:46 +0200

    fuctions expressions vs functions declration

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// x = 100;
+// console.log(x);
+
+// functions
+
+const f = function printName(theName) {
+  // console.log(theName);
+  return theName;
+};
+
+// console.log(f("dc"));
+
+let d = f(44444);
+
+console.log(d);
